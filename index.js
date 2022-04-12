208615cc8b3c1be1559df609f6d108012277fe2c
Lucas Santos <lucas.parkl@outlook.com>
2022-04-12 19:53:29 -0300

    WIP: Aplicações Serveless Lambda Alura - modulo 2: - criando o index.js local - baixando o modulo moment e compactando junto do index.js - deploy via interface para o lambda e deploy via AWS CLI

---
+++ index.js
@@ -0,0 +1,43 @@
+const moment = require('moment')
+
+const pacientes = [
+    {id: 1 ,nome: "Robin", dataNascimento : "1984-01-11"},
+    {id: 2 ,nome: "Batman", dataNascimento : "1986-02-05"},
+    {id: 3 ,nome:"Superman", dataNascimento : "1990-04-20"},
+    {id: 4 ,nome: "Doutor estranho", dataNascimento : "2000-11-03"}
+    ];
+
+
+function buscarPaciente(id){
+    return pacientes.find(paciente => paciente.id == id)
+}
+
+function calcularIdade(paciente){
+    const diaAtual = moment()
+    const dataNascimentoFormatada = moment(paciente.dataNascimento,"YYYY-MM-DD")
+    return diaAtual.diff(dataNascimentoFormatada, 'years')
+}
+
+exports.handler = async (event) => {
+    // TODO implement
+    console.log("Executando Lambda na aws utilizando s3")
+    console.log("id informado: "+event.pacienteId )
+    
+    let pacienteEncontrado
+    if(event.pacienteId){
+        pacienteEncontrado = buscarPaciente(pacienteId)
+        pacienteEncontrado.idade = calcularIdade(pacienteEncontrado)
+        
+        return {
+        statusCode: 200,
+        body: JSON.stringify(pacienteEncontrado),
+    };
+    }
+    
+    const todosPacientes = pacientes.map(paciente => ({...paciente,idade: calcularIdade(p)}))
+    
+    return {
+        statusCode: 200,
+        body: JSON.stringify(todosPacientes),
+    };
+};
