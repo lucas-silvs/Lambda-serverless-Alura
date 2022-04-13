914fd55e1db683497b78a423c91ac6e1fc8c81fc
Lucas Santos <lucas.parkl@outlook.com>
2022-04-13 17:49:28 -0300

    WIP: Aplicações Serveless Lambda Alura - finalização do modulo 3: - configuração para utilizar o API Gateway da AWS para construir uma API Rest e utilizar como trigger para o lambda function - utilizando path parameters para informar o ID do usuario é retornar apenas o usuario do ID informado - tratamento de erro caso o ID não pertença a nenhum usuario da base - Consulta dos logs do cloudwatch via o terminal ao vivo - execução da aplicação localmente utilizando o plugin do Serveless Framework serverless-offline

--- cadastro-pacientes/handler.js
+++ cadastro-pacientes/handler.js
@@ -16,13 +16,16 @@ module.exports.hello = async (event) => {
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
+function buscarnaListaPaciente(id) {
+  return pacientes.find(paciente => paciente.id == id)
+}
 
 const pacientes = [
-  {id: 1 ,nome: "Robin", dataNascimento : "1984-01-11"},
-  {id: 2 ,nome: "Batman", dataNascimento : "1986-02-05"},
-  {id: 3 ,nome:"Superman", dataNascimento : "1990-04-20"},
-  {id: 4 ,nome: "Doutor estranho", dataNascimento : "2000-11-03"}
-  ];
+  { id: 1, nome: "Robin", dataNascimento: "1984-01-11" },
+  { id: 2, nome: "Batman", dataNascimento: "1986-02-05" },
+  { id: 3, nome: "Superman", dataNascimento: "1990-04-20" },
+  { id: 4, nome: "Doutor estranho", dataNascimento: "2000-11-03" }
+];
 
 module.exports.listarPacientes = async event => {
   console.log(event)
@@ -34,6 +37,27 @@ module.exports.listarPacientes = async event => {
       }
     )
   }
-  
+}
 
+module.exports.buscarPaciente = async event => {
+  console.log(event)
+  let pacienteEncontrado
+  const { pacienteId } = event.pathParameters;
+  pacienteEncontrado = buscarnaListaPaciente(pacienteId)
+  if(pacienteEncontrado == undefined){
+
+    return {
+      statusCode: 404,
+      body: JSON.stringify(
+        {
+          "errorMessage": "Paciente não existe"
+        }
+      )
+    }
+  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify(pacienteEncontrado),
+  };
+  
 }
