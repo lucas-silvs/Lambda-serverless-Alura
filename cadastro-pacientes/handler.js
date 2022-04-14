87213adadf1d79fad9949cea67765190eb5cd02a
Lucas Santos <lucas.parkl@outlook.com>
2022-04-14 16:37:30 -0300

    WIP: Aplicações Serveless Lambda Alura - finalização do  modulo 4: - criação de tabelas no DynamoDb pela interface e utilizando o Serverless Framework - realização da busca de todos os usuario, buscar usuario por id e cadastro de usuario com criação de id dinamica - add configuração de permissão da role para realização das operações no DynamoDB

--- cadastro-pacientes/handler.js
+++ cadastro-pacientes/handler.js
@@ -1,11 +1,11 @@
-'use strict';
+"use strict";
 
 module.exports.hello = async (event) => {
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
+        message: "Go Serverless v1.0! Your function executed successfully!",
         input: event,
       },
       null,
@@ -16,48 +16,126 @@ module.exports.hello = async (event) => {
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
-function buscarnaListaPaciente(id) {
-  return pacientes.find(paciente => paciente.id == id)
-}
 
 const pacientes = [
   { id: 1, nome: "Robin", dataNascimento: "1984-01-11" },
   { id: 2, nome: "Batman", dataNascimento: "1986-02-05" },
   { id: 3, nome: "Superman", dataNascimento: "1990-04-20" },
-  { id: 4, nome: "Doutor estranho", dataNascimento: "2000-11-03" }
+  { id: 4, nome: "Doutor estranho", dataNascimento: "2000-11-03" },
 ];
 
-module.exports.listarPacientes = async event => {
-  console.log(event)
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        pacientes
-      }
-    )
-  }
+const AWS = require("aws-sdk");
+const {v4:uuidv4} = require("uuid")
+const moment = require("moment");
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const params = {
+  TableName: "PACIENTES",
+};
+
+function buscarnaListaPaciente(id) {
+  return pacientes.find((paciente) => paciente.id == id);
 }
 
-module.exports.buscarPaciente = async event => {
-  console.log(event)
-  let pacienteEncontrado
-  const { pacienteId } = event.pathParameters;
-  pacienteEncontrado = buscarnaListaPaciente(pacienteId)
-  if(pacienteEncontrado == undefined){
+module.exports.listarPacientes = async (event) => {
+  try {
+    let dados = await dynamoDb.scan(params).promise();
+    console.log(event);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(dados.Items),
+    };
+  } catch (err) {
+    console.log(event);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: err.name ? err.name : "Exception",
+        message: err.message ? err.message : "Unknown error",
+      }),
+    };
+  }
+};
+
+module.exports.buscarPaciente = async (event) => {
+  try {
+    const { pacienteId } = event.pathParameters;
+    //Essa linha funciona semelhante a uma consulta por Id em SQL: select * from PACIENTES where paciemte_id = pacienteId
+    const results = await dynamoDb
+      .get({
+        ...params,
+        Key: {
+          paciente_id: pacienteId,
+        },
+      })
+      .promise();
+    if (!results.Item) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify(
+          {
+            error:
+              "Paciente com id " + pacienteId + " não existe na base de dados",
+          },
+          null,
+          2
+        ),
+      };
+    }
+    const paciente = results.Item;
 
     return {
-      statusCode: 404,
-      body: JSON.stringify(
-        {
-          "errorMessage": "Paciente não existe"
-        }
-      )
-    }
+      statusCode: 200,
+      body: JSON.stringify(paciente),
+    };
+  } catch (err) {
+    console.log(event);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: err.name ? err.name : "Exception",
+        message: err.message ? err.message : "Unknown error",
+      }),
+    };
   }
-  return {
-    statusCode: 200,
-    body: JSON.stringify(pacienteEncontrado),
-  };
-  
-}
+};
+
+module.exports.cadastrarPaciente = async (event) => {
+  try {
+    const payload = JSON.parse(event.body);
+    const dataCriacao = new Date().getTime();
+
+    const {  nome, data_nascimento, email, telefone } = payload;
+
+    const paciente = {
+      paciente_id: uuidv4(),
+      nome,
+      data_nascimento,
+      email,
+      telefone,
+      status: true,
+      criado_em: dataCriacao,
+      atualizado_em: dataCriacao,
+    };
+
+    await dynamoDb
+      .put({
+        TableName: "PACIENTES",
+        Item: paciente,
+      })
+      .promise();
+
+    return {
+      statusCode: 201,
+    };
+  } catch (err) {
+    console.log(event);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: err.name ? err.name : "Exception",
+        message: err.message ? err.message : "Unknown error",
+      }),
+    };
+  }
+};
